6b1d03683d6e9c9e1f7229ac0d8a7357105cffd7
Jessica Dafforn <jdafforn072@gmail.com>
2016-04-06 18:34:48 -0400

    finished

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 var one = $("#1");
 var two = $("#2");
+var three = $("#3");
+var four = $("#4");
 
 one.hover(function() {
     one.fadeTo( "slow" , 0.25);
@@ -13,9 +15,31 @@ two.hover(function() {
 },function(){
     two.fadeTo( "slow" , 1);
 });
-
+three.hover(function() {
+    three.fadeTo( "slow" , 0.25);
+},function(){
+    three.fadeTo( "slow" , 1);
+});
+four.hover(function() {
+    four.fadeTo( "slow" , 0.25);
+},function(){
+    four.fadeTo( "slow" , 1);
+});
+one.click(function(){
+    one.css("background-color","red"),
+    one.add('<span>Clicked</span>').text("Clicked");
+});
 two.click(function(){
-    two.css("background-color","red").after('Clicked');
+    two.css("background-color","red"),
+    two.add('<span>Clicked</span>').text("Clicked");
+});
+three.click(function(){
+    three.css("background-color","red"),
+    three.add('<span>Clicked</span>').text("Clicked");
+});
+four.click(function(){
+    four.css("background-color","red"),
+    four.add('<span>Clicked</span>').text("Clicked");
 });
 
 });
